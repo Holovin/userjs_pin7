8bf8de7f8cd6a16867aa2b90bd6101681b48219b
Alexander Holovin <dvjmixxxer@gmail.com>
2019-11-12 14:52:30 +0300

    Highlight good/bad results

--- pin7.user.js
+++ pin7.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pin7Marker
 // @namespace    http://holov.in/Pin7Marker
-// @version      0.2
+// @version      0.3
 // @description  Userjs for mark viewed rows
 // @author       Alexander Holovin
 // @match        http://pin7.ru/*
@@ -25,7 +25,7 @@
 })();
 
 function modifyElement(id, data) {
-    console.log(id, data);
+    let newData = data;
 
     const isViewedMarkElement = document.createElement('div');
     isViewedMarkElement.setAttribute('data-id', id);
@@ -33,18 +33,41 @@ function modifyElement(id, data) {
     if (data !== 'VISIBLE' && data !== 'HIDDEN') {
         isViewedMarkElement.setAttribute('data-data', 'VISIBLE');
         GM.setValue(`${id}toggle`, 'VISIBLE').then();
+        newData = 'VISIBLE';
 
     } else {
         isViewedMarkElement.setAttribute('data-data', data);
     }
 
     isViewedMarkElement.addEventListener('click', toggleVisible);
-    isViewedMarkElement.innerText = getText(data);
+    isViewedMarkElement.innerText = getText(newData);
 
     const parent = document.querySelector(`#${id} .tdm_09`);
     parent.appendChild(isViewedMarkElement);
-    parent.parentElement.style.opacity = getOpacity(data);
+    parent.parentElement.style.opacity = getOpacity(newData);
     parent.style.wordBreak = 'break-all';
+
+
+    const isStudio = document.querySelector(`#${id} .tdm_01`).innerText.indexOf('Студия') !== -1;
+    if (isStudio) {
+        document.querySelector(`#${id} .tdm_01`).style.backgroundColor = '#FFF1F0';
+    }
+
+    const undergroundRule = new RegExp('((\\d{2,3}\\sм)|([12]\\sкм)|(1\\.\\d\\sкм)).?$', 'gm');
+    const isOkUnderground = undergroundRule.test(document.querySelector(`#${id} .tdm_03`).innerText);
+    if (!isOkUnderground) {
+        document.querySelector(`#${id} .tdm_03`).style.backgroundColor = '#FFF1F0';
+    }
+
+    const areaRule = new RegExp('^(\\d+(\\.\\d{1,2})?)\\/(\\d+|\\?)');
+    const isOkArea = areaRule.exec(document.querySelector(`#${id} .tdm_04`).innerText)[1] > 30;
+    if (!isOkArea) {
+        document.querySelector(`#${id} .tdm_04`).style.backgroundColor = '#FFF1F0';
+    }
+
+    if (!isStudio && isOkUnderground && isOkArea) {
+        document.querySelector(`#${id} .tdm_11`).style.backgroundColor = '#C1FFDA';
+    }
 }
 
 function toggleVisible(event) {
